d651b198a9581add6c0cab22574ecc714fa9c00a
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2021-02-23 16:18:02 +0100

    started product grid

--- src/App.js
+++ src/App.js
@@ -3,36 +3,55 @@ import React, { Component } from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import ProductGrid from "./Components/ProductGrid";
-import FrontPage from './Components/FrontPage';
-import SignIn from './Components/SignIn';
-import SignUp from './Components/SignUp';
+import FrontPage from "./Components/FrontPage";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
 
 class App extends Component {
   state = {
-    productList: [],
+    productList: [
+      {
+        Name: "Trästol",
+        Description:
+          "En fin handtillverkad trästol i gustaviansk stil med robust finish. Tillverkad i ek",
+        Price: 2400,
+      },
+      {
+        Name: "Matbord",
+        Description:
+          "En fin handtillverkad trästol i gustaviansk stil med robust finish. Tillverkad i ek",
+        Price: 2400,
+      },
+      {
+        Name: "Gungstol",
+        Description:
+          "En fin handtillverkad trästol i gustaviansk stil med robust finish. Tillverkad i ek",
+        Price: 2400,
+      },
+    ],
     showSignIn: false,
     showSignUp: false,
     showProducts: false,
   };
 
   render() {
-    const{
-      productList,
-      showProducts,
-      showSignIn,
-      showSignUp
-    } = this.state
+    const { productList, showProducts, showSignIn, showSignUp } = this.state;
 
-    return(
-    <div className="Container">
-      <Header />
-      { showProducts ? (<ProductGrid products={productList}/>)
-      : showSignIn ?  (<SignIn/>) 
-      : showSignUp ?  (<SignUp/>) 
-      : (<FrontPage/>)}
-      <Footer />
-    </div>
-    )
+    return (
+      <div className="Container">
+        <Header />
+        {showProducts ? (
+          <ProductGrid products={productList} />
+        ) : showSignIn ? (
+          <SignIn />
+        ) : showSignUp ? (
+          <SignUp />
+        ) : (
+          <FrontPage />
+        )}
+        <Footer />
+      </div>
+    );
   }
 }
 
--- src/Components/ProductGrid.jsx
+++ src/Components/ProductGrid.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+const ProductCard = (props) => {
+    const Cards = props.products.map((product) => {
+        return (
+            <div className="card">
+                <img src="" alt="Product Image"/>
+                <div className="card-body">
+                    <h3>{product.Name}</h3>
+                    <p>{product.Description}</p>
+                    <b>Pris: {product.Price}</b>
+                    <a className="btn button-outline-primary">Lägg i varukorg</a>
+                </div>
+            </div>
+        )
+    })
+}
 
+const Grid = (props) => {
 
-const GridRow = (props) => {
     return 
 }
 
